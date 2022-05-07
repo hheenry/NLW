bb02ec1758946cec1819a6d88ce412fee915d625
Henry Rodrigues <henry.rodrigues@flcompy.com>
2022-05-07 10:13:23 -0300

    Delete src directory

--- src/repositories/TagsRepositories.ts
+++
@@ -1,7 +0,0 @@
-import {EntityRepository, Repository} from "typeorm";
-import {Tag} from "../entities/tags";
-
-@EntityRepository(Tag)
-class TagsRepository extends Repository<Tag> {}
-
-export {TagsRepository};
